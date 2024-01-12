266482a42f87cd6708be323ad9975b559737718f
Sagar Kumar <Sagar-138>
2024-01-12 23:59:47 +0530

    Cake-n-Bakery

---
+++ js/velvetcake.js
@@ -0,0 +1,10 @@
+
+ 
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 5.2,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
